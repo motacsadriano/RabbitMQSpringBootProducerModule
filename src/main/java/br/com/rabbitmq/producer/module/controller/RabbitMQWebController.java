da463d9fc443dc17df592b34b767aff747455b91
Adriano Mota <motacsadriano@gmail.com>
2020-06-28 20:12:56 -0300

    Finshing with GET and Post Methods Working

--- src/main/java/br/com/rabbitmq/producer/module/controller/RabbitMQWebController.java
+++ src/main/java/br/com/rabbitmq/producer/module/controller/RabbitMQWebController.java
@@ -3,6 +3,8 @@ package br.com.rabbitmq.producer.module.controller;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,12 @@ public class RabbitMQWebController {
 		emp.setSalary(salary);
 
 		amqpTemplate.convertAndSend("pocExchange", "poc", emp);
+		return "Message sent to the RabbitMQ Successfully";
+	}
+	
+	@PostMapping(value = "/producer")
+	public String producerPost(@RequestBody Employee employee) {
+		amqpTemplate.convertAndSend("pocExchange", "poc", employee);
 		return "Message sent to the RabbitMQ Successfully";
 	}
 }
